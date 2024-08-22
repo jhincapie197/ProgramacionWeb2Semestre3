8756096201e5b6446a76b2bceb3b48ea51b26f6c
jhincapie197 <145515467+jhincapie197@users.noreply.github.com>
2024-08-22 18:48:48 -0500

    Terminación actividad crud
    Se termina el formulario de listado de empleados

--- crudLocalStorage/dashboard.js
+++ crudLocalStorage/dashboard.js
@@ -45,3 +45,26 @@ function guardarDatos(datos){
     localStorage.setItem("empleados", JSON.stringify(empleados));
     alert("Datos guardados con éxito");
 }
+
+// Función buscar pedidos
+function buscarPedidos() {
+    let filtro = buscadorInput.value.toLowerCase();
+    let filas = d.querySelectorAll(".table tbody tr");
+    let primerResultado = true;  // Bandera para seleccionar automáticamente el primer resultado coincidente
+    filas.forEach((fila) => {
+        let columnas = fila.querySelectorAll("td");
+        let cliente = columnas[1].innerText.toLowerCase(); // Asumiendo que la columna del cliente es la segunda columna
+        if (cliente.startsWith(filtro)) {
+            fila.style.display = "";
+            if (primerResultado) {
+                fila.classList.add("seleccionado");
+                primerResultado = false;
+            } else {
+                fila.classList.remove("seleccionado");
+            }
+        } else {
+            fila.style.display = "none";
+            fila.classList.remove("seleccionado");
+        }
+    });
+}
